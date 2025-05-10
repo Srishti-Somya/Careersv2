6b2352152f073e85d574a1d533eb78a37295928b
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2025-05-10 06:18:30 +0000

    added changes

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route(
-    "/")  # decorator added when url / is accessed then show hello_world()
+@app.route("/")  # decorator added when url / is accessed then show hello_world()
 def hello_world():
-  return "Hello, Srishti!"
+  return render_template("home.html")
 
 
 if __name__ == "__main__":
