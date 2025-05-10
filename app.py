cabbb972a2ab453dbab32e9b4ec335999e3ce8c6
Srishti-Somya <115712783+Srishti-Somya@users.noreply.github.com>
2025-05-10 09:04:58 +0000

    learned about dynamic route and api

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+PROJECTS = [
+  {
+    'id' : 1,
+    'title': 'RustDB',
+    'completed': 'Ongoing',
+  },
+  {
+    'id' : 2,
+    'title': 'AI Data Curator',
+    'completed': 'Nov 2024',
+  },
+  {
+    'id' : 3,
+    'title': 'Language Translator',
+    'completed': 'Dec 2023',
+  },
+]
 
 
 @app.route("/")  # decorator added when url / is accessed then show hello_world()
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html", projects = PROJECTS)
 
+@app.route("/api/projects")
+def list_projects():
+  return jsonify(PROJECTS)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
